a702c8baff662cb6eb32be8f4087933ffc071b3d
Ramazan Tulanbaev <ramazantulanbaev12@gmail.com>
2021-01-26 17:23:32 +0600

    added and filled in Json.json

--- src/com/company/RJson.java
+++ src/com/company/RJson.java
@@ -1,4 +1,26 @@
 package com.company;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class RJson {
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    public static RJson[] truckReader() {
+        String fromJson = "";
+        try (
+                FileReader fr = new FileReader("DatabaseValue.json");
+                Scanner scan = new Scanner(fr)
+        ) {
+            while (scan.hasNextLine()) {
+                fromJson += scan.nextLine();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return gson.fromJson(fromJson, RJson[].class);
+    }
 }
