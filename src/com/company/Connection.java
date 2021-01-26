75a8253b0afc6e94ad43d686c60c420a9348930c
Ramazan Tulanbaev <ramazantulanbaev12@gmail.com>
2021-01-26 15:22:11 +0600

    added methods to the interface

--- src/com/company/Connection.java
+++ src/com/company/Connection.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public interface Connection {
+    void openADatabaseConnection();
+    void closeTheDatabaseConnection();
+    boolean checkIfThereIsAConnectionToTheBase();
+    void readOneRecordByIndex();
+    boolean checkIfThereIsAnEntryForSuchAndSuchAKey();
+    void readOneRecordByKey();
+    void readACertainSetOfRecordsFromTheDatabase();
+    void findOutTheNumberOfRecordsInTheDatabase();
+    void addRecordToBase();
+    void updateRecordValueByIndex();
+    void updateRecordContentByKey();
 }
